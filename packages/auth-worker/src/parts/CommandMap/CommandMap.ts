46e5019539bcc53848e7c1504c372c769a8e7ec6
Le Vivilet <levivilet@pm.me>
2026-04-02 18:00:29 +0200

    fix

--- packages/auth-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/auth-worker/src/parts/CommandMap/CommandMap.ts
@@ -1,11 +1,7 @@
-import { executeChatTool } from '../ExecuteChatTool/ExecuteChatTool.ts'
-import { getBasicChatTools } from '../GetBasicChatTools/GetBasicChatTools.ts'
 import { handleMessagePort } from '../HandleMessagePort/HandleMessagePort.ts'
 import { initialize } from '../Initialize/Initialize.ts'
 
 export const commandMap = {
-  'ChatTool.execute': executeChatTool,
-  'ChatTool.getTools': getBasicChatTools,
-  'ChatTool.initialize': initialize,
+  'AUth.initialize': initialize,
   'HandleMessagePort.handleMessagePort': handleMessagePort,
 }
