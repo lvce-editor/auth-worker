e120b6839cdbe545dbb3a9b988084d15e4c54103
Le Vivilet <levivilet@pm.me>
2026-04-02 18:02:02 +0200

    fix

--- packages/auth-worker/src/parts/Logout/Logout.ts
+++ packages/auth-worker/src/parts/Logout/Logout.ts
@@ -1,16 +1,9 @@
-import { RendererWorker } from '@lvce-editor/rpc-registry'
-import type { ChatState } from '../ChatState/ChatState.ts'
 import { getLoggedOutBackendAuthState, logoutFromBackend } from '../BackendAuth/BackendAuth.ts'
 
-export const logout = async (state: ChatState): Promise<ChatState> => {
-  const loggingOutState: ChatState = {
-    ...state,
+export const logout = async (state: any): Promise<any> => {
+  const loggingOutState = {
     authErrorMessage: '',
     userState: 'loggingOut',
-  }
-  if (state.uid) {
-    set(state.uid, state, loggingOutState)
-    await RendererWorker.invoke('Chat.rerender')
   }
   await logoutFromBackend(state.backendUrl)
   return {
