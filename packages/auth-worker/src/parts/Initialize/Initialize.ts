4c600c400113fc867b58bf6c41acd89bb38e0558
Le Vivilet <levivilet@pm.me>
2026-04-02 18:04:08 +0200

    fix

--- packages/auth-worker/src/parts/Initialize/Initialize.ts
+++ packages/auth-worker/src/parts/Initialize/Initialize.ts
@@ -1,6 +1,3 @@
-import { initializeTerminalProcess } from '../InitializeTerminalProcess/InitializeTerminalProcess.ts'
-
 export const initialize = async (platform: number): Promise<void> => {
   // TODO
-  await initializeTerminalProcess(platform)
 }
