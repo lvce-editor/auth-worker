b019e87922f96c39b9150efc87f4901e6a5002e2
Le Vivilet <levivilet@pm.me>
2026-04-02 18:04:42 +0200

    fix

--- packages/auth-worker/src/parts/Listen/Listen.ts
+++ packages/auth-worker/src/parts/Listen/Listen.ts
@@ -1,5 +1,3 @@
-import { initializeChatStorageWorker } from '../InitializeChatStorageWorker/InitializeChatStorageWorker.ts'
-import { initializeFileSystemWorker } from '../InitializeFileSystemWorker/InitializeFileSystemWorker.ts'
 import { initializeRendererWorker } from '../InitializeRendererWorker/InitializeRendererWorker.ts'
 
 export const listen = async (): Promise<void> => {
