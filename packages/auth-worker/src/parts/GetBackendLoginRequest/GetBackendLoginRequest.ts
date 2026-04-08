25fb71d6a5d3d76b57370a8aa87370fc7aa08bb1
Le Vivilet <levivilet@pm.me>
2026-04-08 22:06:06 +0200

    fix

--- packages/auth-worker/src/parts/GetBackendLoginRequest/GetBackendLoginRequest.ts
+++ packages/auth-worker/src/parts/GetBackendLoginRequest/GetBackendLoginRequest.ts
@@ -1,4 +1,3 @@
-import { PlatformType } from '@lvce-editor/constants'
 import { createPkceValues } from '../CreatePkceValues/CreatePkceValues.ts'
 import { getBackendAuthUrl } from '../GetBackendAuthUrl/GetBackendAuthUrl.ts'
 import { getEffectiveRedirectUri } from '../GetEffectiveRedirectUri/GetEffectiveRedirectUri.ts'
@@ -15,29 +14,18 @@ export interface BackendLoginRequest {
 
 export const getBackendLoginRequest = async (backendUrl: string, platform = 0, uid = 0, redirectUri = ''): Promise<BackendLoginRequest> => {
   const effectiveRedirectUri = await getEffectiveRedirectUri(platform, uid, redirectUri)
-  if (platform === PlatformType.Electron) {
-    const { codeChallenge, codeVerifier, nonce, state } = await createPkceValues()
-    const loginUrl = new URL(getBackendAuthUrl(backendUrl, '/oidc/auth'))
-    loginUrl.searchParams.set('client_id', oidcClientId)
-    loginUrl.searchParams.set('code_challenge', codeChallenge)
-    loginUrl.searchParams.set('code_challenge_method', 'S256')
-    loginUrl.searchParams.set('nonce', nonce)
-    loginUrl.searchParams.set('redirect_uri', effectiveRedirectUri)
-    loginUrl.searchParams.set('response_type', 'code')
-    loginUrl.searchParams.set('scope', oidcScope)
-    loginUrl.searchParams.set('state', state)
-    return {
-      codeVerifier,
-      loginUrl: loginUrl.toString(),
-      redirectUri: effectiveRedirectUri,
-    }
-  }
-  const loginUrl = new URL(getBackendAuthUrl(backendUrl, '/login'))
-  if (effectiveRedirectUri) {
-    loginUrl.searchParams.set('redirect_uri', effectiveRedirectUri)
-  }
+  const { codeChallenge, codeVerifier, nonce, state } = await createPkceValues()
+  const loginUrl = new URL(getBackendAuthUrl(backendUrl, '/oidc/auth'))
+  loginUrl.searchParams.set('client_id', oidcClientId)
+  loginUrl.searchParams.set('code_challenge', codeChallenge)
+  loginUrl.searchParams.set('code_challenge_method', 'S256')
+  loginUrl.searchParams.set('nonce', nonce)
+  loginUrl.searchParams.set('redirect_uri', effectiveRedirectUri)
+  loginUrl.searchParams.set('response_type', 'code')
+  loginUrl.searchParams.set('scope', oidcScope)
+  loginUrl.searchParams.set('state', state)
   return {
-    codeVerifier: '',
+    codeVerifier,
     loginUrl: loginUrl.toString(),
     redirectUri: effectiveRedirectUri,
   }
