ac26a0ffca66fae4c87234d23863a1954df924ba
Le Vivilet <levivilet@pm.me>
2026-05-01 09:24:18 +0200

    fix

--- packages/auth-worker/src/parts/IsLoginResponse/IsLoginResponse.ts
+++ packages/auth-worker/src/parts/IsLoginResponse/IsLoginResponse.ts
@@ -1,8 +1,5 @@
 import type { LoginResponse } from '../LoginResponse/LoginResponse.ts'
 
 export const isLoginResponse = (value: unknown): value is LoginResponse => {
-  if (!value || typeof value !== 'object') {
-    return false
-  }
-  return true
+  return !!value && typeof value === 'object'
 }
